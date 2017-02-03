14829da9bed647a42f68c71a17da6291e962778a
陈 荫华 <chenyinhua@fucku.local>
2017-02-03 11:17:41 +0800

    Initial Commit

---
+++ PCPhotoPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PCPhotoPicker
+//
+//  Created by 陈 荫华 on 2017/2/3.
+//  Copyright © 2017年 陈 荫华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PCPhotoPicker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PCPhotoPicker
+//
+//  Created by 陈 荫华 on 2017/2/3.
+//  Copyright © 2017年 陈 荫华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
